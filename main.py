ce274941f0316b47cf433928cf0086c3339e7131
Nathaniel Tyler <148269285+natetyty22@users.noreply.github.com>
2025-05-13 12:10:34 -0400

    completed our menu and linked to main file

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import pygame                                   #loading pygame library
+from menu import show_menu                      #import the show_menu function from menu.py
+
 pygame.init()                                   #initializes all the modules 
 pygame.display.set_caption("Space Invaders Remastered") #the window name
 screen = pygame.display.set_mode((800, 800)) #set_mode has the job of creating a surface object to draw on
 gameRunning = True
 clock = pygame.time.Clock()                  #created a clock object
-while gameRunning:
-    screen.fill((0, 0, 128))                 #make the background dark blue
 
-    for event in pygame.event.get():         #the get function comes from the event submodule and lists all the events
-        if event.type == pygame.QUIT:        #event is an instance of Event class, type is an attribue of it
-            gameRunning = False              #close the loop
-    pygame.display.update()
-    clock.tick(60)
+choice = show_menu(screen)                   #pass screen as arg for show_menu function
 
-pygame.quit()
+
+if choice == "start":                       
+    gameRunning = True                      #continue game loop 
+    while gameRunning:                      #game loop
+        screen.fill((0, 0, 128))            #dark blue background
+        for event in pygame.event.get():    #search event list
+            if event.type == pygame.QUIT:   #quit event found, program exits 
+                gameRunning = False         #game loop ends
+        
+        pygame.display.flip()               #update screen
+        clock.tick(60)                      #fps update on clock object
+
+if choice == "quit":                        #"quit" button selected
+    pygame.quit()
--- menu.py
+++ menu.py
@@ -6,3 +6,27 @@ def show_menu(screen):
 
     startText = menuFont.render("Start", True, (255, 255, 255)) #render takes our font object and creates a surface object that is text
     quitText = menuFont.render("Quit", True, (255, 255, 255))
+
+    startCenter = startText.get_rect(center=(400, 600)) #creating a rect object from our newly made surface object
+    quitCenter = quitText.get_rect(center=(400, 400))
+
+    #the menu loop
+    while True:                     
+        screen.fill((0, 0, 128)) #navyblue background for screen object
+
+        #blit(source, destination)   blit(surface object, rect object), drawing a rect object on a surface object
+        screen.blit(startText, startCenter)
+        screen.blit(quitText, quitCenter)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return "quit"
+            
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if startCenter.collidepoint(event.pos):      #event.pos is part of the dictionary attributes of the event object
+                    return "start"                           #these attributes are somewhat like private member variables in c++, but theyre public
+                elif quitCenter.collidepoint(event.pos):     #the event.pos returns a set of coordinates that are checked against the coordinates of the rect obj
+                    return "quit"
+
+        pygame.display.flip()   #runs at the end of the loop and updates the frame of the screen, a method of the clock obj we made above
+        menuClock.tick(60)
